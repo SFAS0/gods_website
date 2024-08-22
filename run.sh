07f6b8c32ba2ee6c4d3b72159341acc41b36390f
SFAS0 <sfas7@mail.ru>
2024-08-23 01:12:20 +0300

    appdate1

--- run.sh
+++ run.sh
@@ -1,7 +1,20 @@
 #!/bin/bash
 
-# Установка зависимостей из requirements.txt
-pip install -r requirements.txt
+# Проверяем, установлен ли Python 3.9
+python3.9 --version > /dev/null 2>&1
+if [[ $? -eq 0 ]]; then
+  echo "Python 3.9 уже установлен."
+else
+  # Устанавливаем Python 3.9
+  echo "Устанавливаем Python 3.9..."
+  sudo apt update
+  sudo apt install python3.9
+fi
+
+# Устанавливаем зависимости из requirements.txt
+echo "Устанавливаем зависимости..."
+pip3.9 install -r requirements.txt
+echo "Установка завершена!"
 
 # Запуск main.py
 python main.py
